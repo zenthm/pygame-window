f4d178b5e86916087efe9b4e5edf2910dff74af3
Zenthm <65337921+zenthm@users.noreply.github.com>
2023-02-13 10:53:49 +0800

    config: add setup.py file
    The setup.py file is used to package and distribute the project.
    
    This commit adds the setup.py file to the project, allowing the project to be
    easily installed and used by others.

---
+++ setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="UTF-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="UTF-8") as f:
+    install_requires = f.read().splitlines()
+
+metadata = {
+    "name": "pygame-window",
+    "version": "1.0.0.dev1",
+    "author": "Zenthm",
+    "author_email": "zenthm.dev@gmail.com",
+    "description": "Enhanced window management and functionalities for Pygame",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "url": "https://pygame-window.readthedocs.io/en/latest/",
+    "download_url": "https://pypi.org/project/pygame-window/",
+    "project_urls": {
+        "Bug Tracker": "https://github.com/zenthm/pygame-window/issues",
+        "Documentation": "https://pygame-window.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/zenthm/pygame-window",
+    },
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries :: pygame",
+    ],
+    "license": "MIT",
+    "packages": find_packages(where="src"),
+    "package_dir": {"": "src"},
+    "install_requires": install_requires,
+    "python_requires": ">=3.7,<3.11",
+    "keywords": [
+        "python",
+        "pygame",
+        "window",
+        "pygame-library",
+        "window-management",
+        "multiple-windows",
+        "pygame-window",
+        "pygame-sdl2",
+    ],
+    "zip_safe": False,
+}
+
+setup(**metadata)
