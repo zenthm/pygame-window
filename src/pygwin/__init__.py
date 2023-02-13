0e50837c242388891c09f8cc8c98e256c7412966
Zenthm <65337921+zenthm@users.noreply.github.com>
2023-02-13 10:56:23 +0800

    feat: add Pygame-Window library
    The Pygame-Window library provides enhanced window management and
    functionalities for Pygame.
    
    This commit adds the Pygame-Window library to the project, implementing the
    core functionality for managing and manipulating multiple windows within the
    Pygame framework.

---
+++ src/pygwin/__init__.py
@@ -0,0 +1,14 @@
+"""
+Pygame-Window: Enhanced window management and functionalities for Pygame
+
+Pygame-Window is a Python library that provides an API for managing and
+manipulating multiple windows within the Pygame framework, as well as enhancing
+their functionalities.
+
+The library uses Pygame's sub-module `pygame._sdl2` to access the low-level
+SDL2 library that Pygame is built upon, providing fine-grained control over
+window creation and management.
+"""
+
+__author__ = "Zenthm"
+__version__ = "1.0.0.dev1"
