76805f5e4443472ed780b931ff0686e40b4c3e39
Zenthm <65337921+zenthm@users.noreply.github.com>
2023-02-14 10:11:08 +0800

    feat: add descriptors and attributes for Window class

---
+++ src/pygwin/_window_descriptors.py
@@ -0,0 +1,108 @@
+"""
+Window Descriptors
+
+This module contains descriptor classes that provide a convenient and
+streamlined interface for accessing and manipulating window properties
+such as title, size, fullscreen, visibility, etc. in a Pygame application.
+
+These descriptors can be used as attributes of the `Window` class to
+manage the behavior and appearance of windows in a Pygame application.
+"""
+
+
+class TitleDescriptor:
+    """Descriptor for the window title."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.title
+
+    def __set__(self, instance, value):
+        instance.__window__.title = value
+
+
+class SizeDescriptor:
+    """Descriptor for the window size."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.size
+
+    def __set__(self, instance, value):
+        instance.__window__.size = value
+
+
+class PositionDescriptor:
+    """Descriptor for the window position."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.position
+
+    def __set__(self, instance, value):
+        instance.__window__.position = value
+
+
+class FullscreenDescriptor:
+    """Descriptor for the fullscreen state of the window."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.fullscreen
+
+    def __set__(self, instance, value):
+        if value:
+            instance.__window__.set_fullscreen(True)
+        else:
+            instance.__window__.set_windowed()
+
+
+class VisibleDescriptor:
+    """Descriptor for the visibility of the window."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.visible
+
+    def __set__(self, instance, value):
+        if value:
+            instance.__window__.show()
+        else:
+            instance.__window__.hide()
+
+
+class BorderlessDescriptor:
+    """Descriptor for the borderless state of the window."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.borderless
+
+    def __set__(self, instance, value):
+        instance.__window__.borderless = not value
+
+
+class ResizableDescriptor:
+    """Descriptor for the resizability of the window."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.resizable
+
+    def __set__(self, instance, value):
+        instance.__window__.resizable = value
+
+
+class MinimizeDescriptor:
+    """Descriptor for the minimized state of the window."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.minimize
+
+    def __set__(self, instance, value):
+        if instance.__dict__["minimized"] != value:
+            instance.__window__.minimize()
+
+
+class MaximizeDescriptor:
+    """Descriptor for the maximized state of the window."""
+
+    def __get__(self, instance, owner):
+        return instance.__window__.maximize
+
+    def __set__(self, instance, value):
+        if instance.__dict__["maximized"] != value:
+            instance.__window__.maximize()
--- src/pygwin/window.py
+++ src/pygwin/window.py
@@ -10,6 +10,8 @@ to make the development process smoother and more efficient.
 from pygame import _sdl2 as sdl
 from pygame._sdl2.video import WINDOWPOS_CENTERED, WINDOWPOS_UNDEFINED
 
+from pygwin import _window_descriptors as windes
+
 
 class Window:
     """A custom `Window` class for Pygame.
@@ -18,7 +20,36 @@ class Window:
     management of windows in Pygame applications. It offers a variety of
     customizable features and convenient window management functions to make
     the development process smoother and more efficient.
+
+    Attributes:
+        title (str): The title of the window, which is displayed in the
+            window's title bar.
+        size (Tuple[int, int]): The width and height of the window, specified
+            as a tuple of integers.
+        position (Union[Tuple[int, int], int]): The position of the window on
+            the screen.
+        fullscreen (bool): Whether the window should be displayed in
+            fullscreen mode.
+        visible (bool): Whether the window is currently visible.
+        borderless (bool): Whether the window has a border.
+        resizable (bool): Whether the user can resize the window.
+        minimized (bool): Whether the window is currently minimized.
+        maximized (bool): Whether the window is currently maximized.
+        __window__ (sdl.video.Window): An instance of `sdl.video.Window`
+            representing the window.
+        __renderer__ (sdl.video.Renderer): An instance of `sdl.video.Renderer`
+            associated with the window.
     """
+
+    title = windes.TitleDescriptor()
+    size = windes.SizeDescriptor()
+    position = windes.PositionDescriptor()
+    fullscreen = windes.FullscreenDescriptor()
+    visible = windes.VisibleDescriptor()
+    borderless = windes.BorderlessDescriptor()
+    resizable = windes.ResizableDescriptor()
+    minimize = windes.MinimizeDescriptor()
+    maximize = windes.MaximizeDescriptor()
 
     __dict__ = dict(
         title="pygame",
