92b3877943bdf8b2ea3f858358313e3d30130430
Zenthm <65337921+zenthm@users.noreply.github.com>
2023-02-14 10:04:55 +0800

    feat: add custom Window class for Pygame

---
+++ src/pygwin/window.py
@@ -0,0 +1,18 @@
+"""
+Custom Window Class for Pygame
+
+This module defines a custom `Window` class for Pygame that streamlines the
+creation and management of windows in Pygame applications. The class offers a
+variety of customizable features and convenient window management functions
+to make the development process smoother and more efficient.
+"""
+
+
+class Window:
+    """A custom `Window` class for Pygame.
+
+    This class is a custom class for Pygame that streamlines the creation and
+    management of windows in Pygame applications. It offers a variety of
+    customizable features and convenient window management functions to make
+    the development process smoother and more efficient.
+    """
