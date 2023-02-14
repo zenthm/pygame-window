05e4e19e2550baa79b02db0c55482b794a1142d5
Zenthm <65337921+zenthm@users.noreply.github.com>
2023-02-14 10:12:57 +0800

    feat: add type annotations for Window class

---
+++ src/pygwin/window.pyi
@@ -0,0 +1,34 @@
+from typing import Tuple, Union
+from pygame import Color
+from pygwin import WINDOWPOS_CENTERED
+
+class Window:
+    title: str
+    size: Tuple[int, int]
+    position: Union[int, Tuple[int, int]]
+    fullscreen: bool
+    visible: bool
+    borderless: bool
+    resizable: bool
+    minimized: bool
+    maximized: bool
+
+    def __init__(
+        self,
+        title: str = "pygame",
+        size: Tuple[int, int] = (640, 480),
+        position: Union[int, Tuple[int, int]] = WINDOWPOS_CENTERED,
+        fullscreen: bool = False,
+        visible: bool = False,
+        borderless: bool = False,
+        resizable: bool = False,
+        minimized: bool = False,
+        maximized: bool = False,
+    ) -> None: ...
+    def fill(self, color: Union[Color, Tuple[int, int, int]] = (0, 0, 0)) -> None: ...
+    def update(self) -> None: ...
+    def hide(self) -> None: ...
+    def show(self) -> None: ...
+    def minimize(self) -> None: ...
+    def maximize(self) -> None: ...
+    def destroy(self) -> None: ...
