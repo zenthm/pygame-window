d5cce3ab4b5b736e5992dfa63ee657f888451d39
Zenthm <65337921+zenthm@users.noreply.github.com>
2023-02-19 10:39:05 +0800

    tests: add testing to the project

---
+++ tests/test_window.py
@@ -0,0 +1,44 @@
+import pygame
+import pygwin
+
+
+def test_attributes():
+    pygame.init()
+    window = pygwin.Window()
+    window.fullscreen = True
+    window.fullscreen = False
+    window.visible = True
+    window.visible = False
+    window.borderless = True
+    window.borderless = False
+    window.resizable = True
+    window.resizable = False
+    window.minimized = True
+    window.minimized = False
+    window.maximized = True
+    window.maximized = False
+
+    window.title
+    window.size
+    window.position
+    window.fullscreen
+    window.visible
+    window.borderless
+    window.resizable
+    window.minimized
+    window.maximized
+    pygame.quit()
+
+
+def test_methods():
+    pygame.init()
+    window = pygwin.Window()
+    window.destroy()
+    window = pygwin.Window()
+    window.fill()
+    window.update()
+    window.hide()
+    window.show()
+    window.minimize()
+    window.maximize()
+    pygame.quit()
